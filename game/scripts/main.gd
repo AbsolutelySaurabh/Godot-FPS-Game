2c1a87373b17176140405f2644decda5a29c3a5b
Saurabh Singh <absolutelysaurabh03@gmail.com>
2018-05-26 00:15:23 +0530

    Update main.gd

--- game/scripts/main.gd
+++ game/scripts/main.gd
@@ -1,6 +1,4 @@
-# Copyright (c) 2015 Calinou
-# This source code form is governed by the MIT license.
-# See LICENSE.md for more information.
+# Copyright (c) 2018 AbsolutelySaurabh
 
 extends Spatial
 
